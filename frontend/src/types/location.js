70c77b6ff1a7bbab28a73c80eda2485a3c3e613d
sakrobinson <scott.austen.robinson@gmail.com>
2024-12-09 12:44:06 -0700

    cards, and backend integration (basic)

---
+++ frontend/src/types/location.js
@@ -0,0 +1,21 @@
+export const ACTIVITIES = {
+  HIKING: 'hiking',
+  CLIMBING: 'climbing',
+  BIKING: 'biking',
+  SKIING: 'skiing',
+  KAYAKING: 'kayaking',
+  FISHING: 'fishing',
+  HUNTING: 'hunting',
+  DOGS: 'dogs',
+};
+
+export const ACTIVITY_LABELS = {
+  [ACTIVITIES.HIKING]: '🥾 Hiking',
+  [ACTIVITIES.CLIMBING]: '🧗‍♂️ Climbing',
+  [ACTIVITIES.BIKING]: '🚴‍♂️ Biking',
+  [ACTIVITIES.SKIING]: '⛷️ Skiing',
+  [ACTIVITIES.KAYAKING]: '🛶 Kayaking',
+  [ACTIVITIES.FISHING]: '🎣 Fishing',
+  [ACTIVITIES.HUNTING]: '🏹 Hunting',
+  [ACTIVITIES.DOGS]: '🐶 Dogs',
+}; 
