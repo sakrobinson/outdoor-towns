09cfb58c41c178e238883973f11a41389f156bea
sakrobinson <scott.austen.robinson@gmail.com>
2025-01-07 14:33:26 -0700

    add locations

--- frontend/src/components/LocationCard.js
+++ frontend/src/components/LocationCard.js
@@ -1,4 +1,20 @@
 import React from 'react';
+import { ACTIVITY_LABELS } from '../types/location';
+
+function ActivityScore({ activity, score }) {
+  return (
+    <div className="activity-score">
+      <span className="activity-label">{ACTIVITY_LABELS[activity]}</span>
+      <div className="score-bar">
+        <div 
+          className="score-fill" 
+          style={{ width: `${score * 20}%` }} // Convert 1-5 to percentage
+        />
+      </div>
+      <span className="score-number">{score}/5</span>
+    </div>
+  );
+}
 
 function LocationCard({ location, onSelect }) {
   return (
@@ -8,8 +24,14 @@ function LocationCard({ location, onSelect }) {
     >
       <h3>{location.name}</h3>
       <p>{location.description}</p>
-      <div className="location-activities">
-        {/* We'll add activity icons/scores here later */}
+      <div className="activities-grid">
+        {Object.entries(location.activities || {}).map(([activity, score]) => (
+          <ActivityScore 
+            key={activity} 
+            activity={activity} 
+            score={score}
+          />
+        ))}
       </div>
     </div>
   );
